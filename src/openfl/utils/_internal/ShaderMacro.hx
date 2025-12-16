f92c9e560322eeb298dee92b83a927f8b93024ce
Vinegar (Moxie) <124418090+moxie-coder@users.noreply.github.com>
2025-12-15 22:35:54 -0500

    Update default glVersion selection logic
    Simplified the glVersion selection by combining android and web cases, and set the default to '120' for other platforms. This helps prevent crashes by ensuring the correct version is chosen based on platform.

--- src/openfl/utils/_internal/ShaderMacro.hx
+++ src/openfl/utils/_internal/ShaderMacro.hx
@@ -440,7 +440,7 @@ class ShaderMacro
 	{
 		// Specify the default glVersion.
 		// We can use compile defines to guess the value that prevents crashes in the majority of cases.
-		return #if (android) "100" #elseif (web) "100" #elseif (mac) "120" #else "100" #end;
+		return #if (android || web) "100" #else "120" #end;
 	}
 
 	/**
